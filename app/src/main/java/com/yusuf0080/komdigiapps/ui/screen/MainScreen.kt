80144cbdc44bd03200f0437047132294857dd339
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-24 16:25:51 +0700

    Mengambil data dari database

--- app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/MainScreen.kt
@@ -22,8 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -36,6 +39,7 @@ import com.yusuf0080.komdigiapps.R
 import com.yusuf0080.komdigiapps.model.Catatan
 import com.yusuf0080.komdigiapps.navigation.Screen
 import com.yusuf0080.komdigiapps.ui.theme.KomdigiAppsTheme
+import com.yusuf0080.komdigiapps.util.ViewModelFactory
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -73,8 +77,10 @@ fun MainScreen(navController: NavHostController){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier, navController: NavHostController) {
-    val viewModel: MainViewModel = viewModel()
-    val data = viewModel.data
+    val context = LocalContext.current
+    val factory = ViewModelFactory(context)
+    val viewModel: MainViewModel = viewModel(factory = factory)
+    val data by viewModel.data.collectAsState()
 
     if (data.isEmpty()) {
         Column(
---
+++ app/src/main/java/com/yusuf0080/komdigiapps/util/ViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.yusuf0080.komdigiapps.util
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.yusuf0080.komdigiapps.database.CatatanDb
+import com.yusuf0080.komdigiapps.ui.screen.MainViewModel
+import kotlin.contracts.Returns
+
+class ViewModelFactory(
+private val context: Context
+) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val  dao = CatatanDb.getInstance(context).dao
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            return MainViewModel(dao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
