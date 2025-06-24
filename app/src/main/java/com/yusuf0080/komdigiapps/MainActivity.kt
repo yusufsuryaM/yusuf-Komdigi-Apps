b571e2f29c44218386cc97763c6375b1c467afde
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-25 02:28:15 +0700

    Membuat Splash Screen

--- app/src/main/java/com/yusuf0080/komdigiapps/MainActivity.kt
+++ app/src/main/java/com/yusuf0080/komdigiapps/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yusuf0080.komdigiapps.navigation.SetupNavGraph
 import com.yusuf0080.komdigiapps.ui.theme.KomdigiAppsTheme
 
@@ -11,6 +12,7 @@ import com.yusuf0080.komdigiapps.ui.theme.KomdigiAppsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             KomdigiAppsTheme {
