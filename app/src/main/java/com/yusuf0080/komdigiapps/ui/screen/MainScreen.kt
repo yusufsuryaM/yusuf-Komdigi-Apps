61e1377d7dad0b5063b3eee039f57318b2483c85
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-24 13:13:30 +0700

    Membuat tampilan list

--- app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/MainScreen.kt
+++ app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/MainScreen.kt
@@ -1,8 +1,14 @@
 package com.yusuf0080.komdigiapps.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -11,8 +17,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yusuf0080.komdigiapps.R
+import com.yusuf0080.komdigiapps.model.Catatan
 import com.yusuf0080.komdigiapps.ui.theme.KomdigiAppsTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,10 +48,38 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn (
+        modifier = modifier.fillMaxSize()
+    ){
+        items(data) {
+            ListItem(catatan = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(catatan: Catatan) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = catatan.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = catatan.catatan,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = catatan.tanggal)
+    }
 }
 
 @Composable
