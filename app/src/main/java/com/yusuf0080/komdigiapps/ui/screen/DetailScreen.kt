3f2fffb89b7d4ee32c42127c2dd866f8847fb940
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-24 23:28:51 +0700

    Membuat dialog konfirmasi hapus

--- app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/yusuf0080/komdigiapps/ui/screen/DetailScreen.kt
@@ -54,6 +54,7 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
 
     var judul by remember { mutableStateOf("") }
     var catatan by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         if (id == null) return@LaunchedEffect
@@ -105,8 +106,7 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
                     }
                     if (id != null) {
                         DeleteAction {
-                            viewModel.delete(id)
-                            navController.popBackStack()
+                            showDialog = true
                         }
                     }
                 }
@@ -120,6 +120,15 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
             onDescChange = { catatan = it },
             modifier = Modifier.padding(padding)
         )
+
+        if (id != null && showDialog) {
+            DisplayAlertDialog(
+                onDismissRequest = { showDialog = false }) {
+                showDialog= false
+                viewModel.delete(id)
+                navController.popBackStack()
+            }
+        }
     }
 }
 
